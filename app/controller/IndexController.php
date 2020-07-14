55ae75a26587892b85c3eb07fcefe426e799de50
江兆辉 <949363409@qq..com>
2020-07-14 18:06:58 +0800

    view 使用调整

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -6,8 +6,10 @@ class IndexController extends BaseController {
     public function test()
     {
         $this->assign('var','这是var的值');
-        $this->assign('data','这是data的值');
-        $this->assign('test','这是test的值');
+        $this->assign('title','这是index的标题');
+
+        $this->assign('users',['user'=>'这是name','name2'=>'这是name','33333','4444']);
+
 
         $this->display('index');
     }
