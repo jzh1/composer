ab12c8612295b408b41f693131ac7e83cbfab077
江兆辉 <949363409@qq..com>
2021-12-31 14:02:03 +0800

    登陆 && doc

---
+++ doc/apis/Common.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * jzh
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is available
+ * through the world-wide-web at this URL: http://www.opensource.org/licenses/OSL-3.0
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to sales@d1m.cn so we can send you a copy immediately.
+ *
+ * @author robin.fu@d1m.cn
+ * @copyright 2012-2019 D1m
+ * @license http://www.opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+
