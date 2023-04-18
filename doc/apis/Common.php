ef34c891114a658f2ad5d02f91d915ddf3f1b93a
江兆辉 <949363409@qq..com>
2023-04-18 14:05:36 +0800

    - remove 邮箱

--- doc/apis/Common.php
+++ doc/apis/Common.php
@@ -7,10 +7,8 @@
  * This source file is subject to the Open Software License (OSL 3.0) that is available
  * through the world-wide-web at this URL: http://www.opensource.org/licenses/OSL-3.0
  * If you are unable to obtain it through the world-wide-web, please send an email
- * to sales@d1m.cn so we can send you a copy immediately.
+ * to  so we can send you a copy immediately.
  *
- * @author robin.fu@d1m.cn
- * @copyright 2012-2019 D1m
  * @license http://www.opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
